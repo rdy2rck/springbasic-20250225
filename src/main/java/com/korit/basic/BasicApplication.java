867e8878ff1c9a758a0bb27927b18b99d304ad81
smithrowe-10 <smj0418smj@gmail.com>
2025-02-25 17:42:07 +0900

    2025-02-25 'Spring Framework 생성, Gradle 생성, 어노테이션' 추가

---
+++ src/main/java/com/korit/basic/BasicApplication.java
@@ -0,0 +1,11 @@
+package com.korit.basic;
+
+import org.springframework.boot.SpringApplication;
+
+public class BasicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BasicApplication.class, args);
+  }
+  
+}
