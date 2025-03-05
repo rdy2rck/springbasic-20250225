8bcca307a4a510fc1ad16e703ef073e69d78a20c
smithrowe-10 <smj0418smj@gmail.com>
2025-03-05 17:48:48 +0900

    2025-03-05 'Query Method, 빌더 패턴, CRUD 메서드, 특정 사용자 조회(GET), 사용자 삭제(DELETE)' 기능 추가

--- src/main/java/com/korit/basic/controller/UserController.java
+++ src/main/java/com/korit/basic/controller/UserController.java
@@ -5,11 +5,15 @@
 package com.korit.basic.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.korit.basic.dto.GetUserResponseDto;
 import com.korit.basic.dto.PostUserRequestDto;
 import com.korit.basic.service.UserService;
 import com.korit.basic.dto.ResponseDto;
@@ -29,6 +33,22 @@ public class UserController {
     @RequestBody @Valid PostUserRequestDto requestBody
   ) {
     ResponseEntity<ResponseDto> response = userService.postUser(requestBody);
+    return response;
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<? super GetUserResponseDto> getUser(
+    @PathVariable("userId") String userId
+  ) {
+    ResponseEntity<? super GetUserResponseDto> response = userService.getUser(userId);
+    return response;
+  }
+
+  @DeleteMapping("/{userId}")
+  public ResponseEntity<ResponseDto> deleteUser(
+    @PathVariable("userId") String userId
+  ) {
+    ResponseEntity<ResponseDto> response = userService.deleteUser(userId);
     return response;
   }
 
